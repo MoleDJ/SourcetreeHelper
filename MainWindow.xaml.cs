fe444161d932f0020852018554f77c9a26920952
Jagoba Los Arcos <moledj@moledj.com>
2015-02-06 18:17:42 +0100

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibGit2Sharp;
+
+namespace SourceTreeHelper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DoExecute();
+        }
+
+        void DoExecute()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length == 1)
+            {
+                MessageBox.Show("Not arguments received");
+                Application.Current.Shutdown();
+            }
+            switch (args[1])
+            {
+                case "OpenGitRepo":
+                    {
+                        var path = args[2];
+                        var repo = new Repository(path);
+                        var url = repo.Config.Get<string>("remote.origin.url").Value;
+                        if (url.EndsWith(".git"))
+                        {
+                            url = url.Substring(0, url.Length - 4);
+                        }
+                        System.Diagnostics.Process.Start(url);
+                        Application.Current.Shutdown();
+                        break;
+                        break;
+                    }
+                case "OpenGitCommit":
+                {
+                    var path = args[2];
+                    var shaCommit = args[3];
+                    var repo = new Repository(path);
+                    var url = repo.Config.Get<string>("remote.origin.url").Value;
+                    if (url.EndsWith(".git"))
+                    {
+                        url = url.Substring(0, url.Length - 4);
+                    }
+                    url += "/commit/" + shaCommit;
+                    System.Diagnostics.Process.Start(url);
+                    Application.Current.Shutdown();
+                    break;
+                }
+                case "UpdateSmartbanner":
+                {
+                    var path = args[2];
+                    var repoDir = System.IO.Directory.GetDirectories(path, "smartbanner", SearchOption.AllDirectories);
+                    if (repoDir.Length == 1)
+                    {
+                        var smartbannerRepoPath = repoDir[0];
+                        var smartbannerPath = System.Configuration.ConfigurationManager.AppSettings["smartbannerPath"];
+                        var bcCompare = System.Configuration.ConfigurationManager.AppSettings["bcPath"];
+                        var arguments = "\"" + smartbannerPath + "\" \"" + smartbannerRepoPath + "\"";
+                        System.Diagnostics.Process.Start(bcCompare, arguments);
+                    }
+                    else
+                    {
+                        MessageBox.Show("This repository does not have smartbanner folder.");
+                    }
+                    Application.Current.Shutdown();
+                    
+                    break;
+                }
+                case "UpdateTTClasses":
+                {
+                    var path = args[2];
+                    var classTTConnectionFiles = System.IO.Directory.GetFiles(path, "classTTConnection.php", SearchOption.AllDirectories);
+                    if (classTTConnectionFiles.Length == 1)
+                    {
+                        var ttlibRepoPath = classTTConnectionFiles[0].Replace("classTTConnection.php","");
+                        var ttlibPath = System.Configuration.ConfigurationManager.AppSettings["ttlibPath"];
+                        var bcCompare = System.Configuration.ConfigurationManager.AppSettings["bcPath"];
+                        var arguments = "\"" + ttlibPath + "\" \"" + ttlibRepoPath + "\"";
+                        System.Diagnostics.Process.Start(bcCompare, arguments);
+                    }
+                    else
+                    {
+                        MessageBox.Show("This repository does not have classTTConnection.php file.");
+                    }
+                    Application.Current.Shutdown();
+
+                    break;
+                }
+            }
+
+        }
+    }
+}
